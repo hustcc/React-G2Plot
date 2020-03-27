a54c205a96af76cf4ac6aaa92fc798c27145da85
hustcc <i@hust.cc>
2020-03-27 13:34:19 +0800

    feat: add demo

---
+++ demo/index.tsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Line } from '@antv/g2plot';
+
+import ReactG2Plot from '../src';
+
+const data = [
+  { year: '1991', value: 3 },
+  { year: '1992', value: 4 },
+  { year: '1993', value: 3.5 },
+  { year: '1994', value: 5 },
+  { year: '1995', value: 4.9 },
+  { year: '1996', value: 6 },
+  { year: '1997', value: 7 },
+  { year: '1998', value: 9 },
+  { year: '1999', value: 13 },
+];
+
+// line chart config
+const config = {
+  title: {
+    visible: true,
+    text: '曲线折线图',
+  },
+  description: {
+    visible: true,
+    text: '用平滑的曲线代替折线。',
+  },
+  padding: 'auto',
+  forceFit: true,
+  data,
+  xField: 'year',
+  yField: 'value',
+  smooth: true,
+};
+
+ReactDOM.render(
+  <ReactG2Plot className="g2plot-for-react" Ctor={Line} config={config} />,
+  document.getElementById('root-container'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    demo: './demo/index.tsx',
+  },
+  output: {
+    filename: '[name].min.js',
+    path: resolve(__dirname, 'public/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: false,
+            compilerOptions: {
+              declaration: false,
+            },
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'demo/index.html', to: 'index.html' },
+    ]),
+  ],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  mode: 'production',
+};
